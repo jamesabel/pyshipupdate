2c68491512edd94eeb16f0911f6d7d4e6f0d7519
JamesAbel <j@abel.co>
2026-06-26 12:26:31 -0700

    ensure AWS bucket name doesn't have spaces

--- pyshipupdate/bucket.py
+++ pyshipupdate/bucket.py
@@ -1,2 +1,6 @@
 def create_bucket_name(target_app_name: str, target_app_author: str) -> str:
-    return f"pyship-{target_app_author}-{target_app_name}"
+    target_app_name = target_app_name.strip()
+    target_app_author = target_app_author.strip()
+    s = f"pyship-{target_app_author}-{target_app_name}"
+    s = s.replace(" ", "-")  # AWS does not allow spaces in bucket names
+    return s
