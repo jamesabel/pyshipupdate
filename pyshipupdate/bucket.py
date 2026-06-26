0e817fa53f59483b0904abb0f6bfbad66d9900da
JamesAbel <j@abel.co>
2026-06-26 12:39:40 -0700

    ensure AWS bucket name is all lower case

--- pyshipupdate/bucket.py
+++ pyshipupdate/bucket.py
@@ -3,4 +3,5 @@ def create_bucket_name(target_app_name: str, target_app_author: str) -> str:
     target_app_author = target_app_author.strip()
     s = f"pyship-{target_app_author}-{target_app_name}"
     s = s.replace(" ", "-")  # AWS does not allow spaces in bucket names
+    s = s.lower()  # AWS buckets must be all lower case
     return s
